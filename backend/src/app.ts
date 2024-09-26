174fc2fdeed4ee58aad95a53fde74274d44552c0
Youssef Yasser <yousef.mostafa@student.guc.edu.eg>
2024-09-26 21:30:13 +0300

    feat: setup mongoose & create location schema

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,9 +2,11 @@ import 'dotenv/config';
 import express from 'express';
 import { routes } from './routes/routes';
 import { logger } from './middlewares/logger';
+import mongoose from 'mongoose';
 
 const app = express();
 const port = process.env.PORT || 3000;
+const mongoConnectionString = process.env.MONGO_URI || '';
 
 app.use(express.json());
 app.use('/api', routes);
@@ -12,3 +14,12 @@ app.use('/api', routes);
 app.listen(port, () => {
   logger.log('info', `Server is Successfully Running, and App is listening on port ${port}`);
 });
+
+mongoose
+  .connect(mongoConnectionString)
+  .then(() => {
+    logger.log('info', 'Connected to MongoDB');
+  })
+  .catch((error) => {
+    logger.error(`Error connecting to MongoDB ${error}`);
+  });
