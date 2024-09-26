c926bd6b1e3fe65705a24b0c04263f893622ce1d
Youssef Yasser <yousef.mostafa@student.guc.edu.eg>
2024-09-26 22:17:33 +0300

    feat: implement fetching an itinerary
    - Refactored folder structure
    - Created custom validationException
    - Created a repository for controllers to communicate with models

--- backend/src/controllers/itineraryController.ts
+++ backend/src/controllers/itineraryController.ts
@@ -1,9 +1,17 @@
 import { Router } from 'express';
+import { getItinerary } from '../database/repositories/itineraryRepo';
+import { logger } from '../middlewares/logger';
 
 const itineraryController = Router();
 
-itineraryController.get('/:id', (req, res) => {
-  res.json({ message: 'Get Itinerary' });
+itineraryController.get('/:id', async (req, res) => {
+  try {
+    const itineraries = await getItinerary(req.params.id);
+    res.json({ message: 'Itinerary fetched successfully', data: itineraries });
+  } catch (error: any) {
+    logger.error(`Error fetching itinerary: ${error.message}`);
+    res.status(400).json({ message: error.message });
+  }
 });
 
 itineraryController.post('/', (req, res) => {
