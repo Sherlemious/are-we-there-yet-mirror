6858013eb8334dfaac83d99260d3e23aa1ccdcfb
Mostafa Hisham <52044516+mostafahisham03@users.noreply.github.com>
2024-10-02 18:00:33 +0300

    feat: admin profile cruds

---
+++ backend/src/routes/admin.route.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { createAdmin, updateAdmin, getAdmins } from "../controllers/users/admin.controller";
+
+const router = Router();
+
+router.get('', getAdmins);
+router.post('', createAdmin);
+router.put('/:id', updateAdmin);
+
+
+export default router;
--- backend/src/routes/user.route.ts
+++ backend/src/routes/user.route.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express';
 import advertiserRouter from './advertiser.route';
 import sellerRouter from './seller.route';
+import adminRouter from './admin.route';
 import { deleteUser, acceptUser, findUserById } from '../controllers/users/user.controller';
 
 const router = Router();
 
 router.use('/advertisers', advertiserRouter);
 router.use('/sellers', sellerRouter);
+router.use('/admins', adminRouter);
 router.delete('/:id', deleteUser);
 router.patch('/:id/accept-user', acceptUser);
 router.get('/:id', findUserById);
