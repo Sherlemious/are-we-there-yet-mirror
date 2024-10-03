54486a6b1b102678a96dc5b351c2cd69ff72d9a5
MarwanSiSi <marwan.elsisi21@gmail.com>
2024-10-03 22:56:54 +0300

    added functionality refactored styling

--- frontend/src/modules/shared/components/Slider.tsx
+++ frontend/src/modules/shared/components/Slider.tsx
@@ -1,47 +1,75 @@
 import { useState, useEffect } from "react";
-import { ChevronLeft, ChevronRight } from "lucide-react";
+import {
+  ChevronLeft,
+  ChevronRight,
+  Minus,
+  Plus,
+  ChevronsRight,
+  ChevronsLeft,
+} from "lucide-react";
+import Button from "./Button";
 
-const works = [
-  {
-    title: "Lorem Ipsum 1",
-    content:
-      "Si osculantur puer tuus aut uxorem tuam, osculum, non dico quod omnia quae sunt hominis, et sic non tangetur, si aut ex eis moriatur.",
-  },
-  {
-    title: "Lorem Ipsum 2",
-    content:
-      "Quando ambulabat agendis admonere te ipsum de vita tua, et de operibus tuis, ut non pecces in lingua tua, et non offendas in labiis tuis.",
-  },
-  {
-    title: "Lorem Ipsum 3",
-    content:
-      "Noli esse incredibilis timori Domini, et ne accesseris ad illum duplici corde. Ne fueris hypocrita in conspectu hominum, et non scandalizeris in labiis tuis.",
-  },
-  {
-    title: "Lorem Ipsum 4",
-    content:
-      "Attende tibi, et doctrine: insta in illis. Hoc enim faciens, et teipsum salvum facies, et eos qui te audiunt.",
-  },
-  {
-    title: "Lorem Ipsum 5",
-    content:
-      "Omnis sapientia a Domino Deo est, et cum illo fuit semper, et est ante aevum. Arenam maris, et pluviae guttas, et dies saeculi quis dinumeravit?",
-  },
-  {
-    title: "Lorem Ipsum 6",
-    content:
-      "Qui navigant mare, enarrant pericula ejus; et audientes auribus nostris admiramur. Illic praeclara opera et mirabilia, varia bestiarum genera, et omnium pecorum, et creatura belluarum. abilia, varia bestiarum genera, et omnium pecorum, et creatura belluarum. abilia, varia bestiarum genera, et omnium pecorum, et creatura belluarum. abilia, varia bestiarum genera, et omnium pecorum, et creatura belluarum. abilia, varia bestiarum genera, et omnium pecorum, et creatura belluarum. abilia, varia bestiarum genera, et omnium pecorum, et creatura belluarum. abilia, varia bestiarum genera, et . ",
-  },
-];
+const customStyles = {
+  container:
+    "h-auto max-w-fit border-2 border-gray-300 pr-14 pt-14 pl-20 pb-14",
+  title: "mb-16 w-fit border-b-2 border-borders-bottomBorder pb-2 text-2xl",
+  sliderContainer: "relative",
+  sliderContent: "overflow-hidden",
+  sliderWrapper: "flex transition-all duration-600 ease-in-out",
+  slide: "w-[25%] flex-shrink-0 px-2 transition-all duration-600",
+  slideContent:
+    "h-full w-80 overflow-auto border-2 border-gray-300 bg-white p-8 relative",
+  slideTitle: "mb-2 font-bold",
+  slideText: "text-sm",
+  navButton:
+    "absolute top-1/2 z-10 -translate-y-1/2 transform rounded-full bg-white bg-opacity-50 p-2 transition-all hover:bg-opacity-75 focus:outline-none focus:ring-2 focus:ring-black",
+  removeButton:
+    "absolute group top-2 right-2 z-10 rounded-full bg-background-button p-1 hover:bg-red-600 focus:outline-none duration-150",
+  addSlideDiv:
+    "flex items-center justify-center h-full w-80 border-2 border-dashed border-gray-300 bg-white cursor-pointer hover:bg-gray-50",
+  addSlideIcon: "text-gray-400 w-16 h-16",
+  navigationButtons: "flex justify-between mt-4",
+  endBeginButton:
+    "px-4 py-2 rounded hover:opacity-70 focus:outline-none flex items-center justify-center disabled:opacity-50 disabled:cursor-not-allowed",
+};
 
-const Slider = ({ title }: { title: string }) => {
+const Slider = ({
+  title,
+  onAddSlide,
+}: {
+  title: string;
+  onAddSlide: () => void;
+}) => {
+  const [works, setWorks] = useState([
+    {
+      title: "Lorem Ipsum 1",
+      content:
+        "Si osculantur puer tuus aut uxorem tuam, osculum, non dico quod omnia quae sunt hominis, et sic non tangetur, si aut ex eis moriatur.et sic non tangetur, si aut ex eis moriaturet sic non tangetur, si aut ex eis moriaturet sic non tangetur, si aut ex eis moriaturet sic non tangetur, si aut ex eis moriaturet sic non tangetur, si aut ex eis moriaturet sic non tangetur, si aut ex eis moriaturet sic non tangetur, si aut ex eis moriaturet sic non tangetur, si aut ex eis moriaturet sic non tangetur, si aut ex eis moriaturet sic non tangetur, si aut ex eis moriaturet sic non tangetur, si aut ex eis moriaturet sic non tangetur, si aut ex eis moriaturet sic non tangetur, si aut ex eis moriaturet sic non tangetur, si aut ex eis moriaturet sic non tangetur, si aut ex eis moriatur",
+    },
+    {
+      title: "Lorem Ipsum 2",
+      content:
+        "Quando ambulabat agendis admonere te ipsum de vita tua, et de operibus tuis, ut non pecces in lingua tua, et non offendas in labiis tuis.",
+    },
+    {
+      title: "Lorem Ipsum 3",
+      content:
+        "Noli esse incredibilis timori Domini, et ne accesseris ad illum duplici corde. Ne fueris hypocrita in conspectu hominum, et non scandalizeris in labiis tuis.",
+    },
+    {
+      title: "Lorem Ipsum 4",
+      content:
+        "Attende tibi, et doctrine: insta in illis. Hoc enim faciens, et teipsum salvum facies, et eos qui te audiunt.",
+    },
+  ]);
+
   const [currentIndex, setCurrentIndex] = useState(0);
   const [isTransitioning, setIsTransitioning] = useState(false);
 
   const nextSlide = () => {
     if (!isTransitioning) {
       setIsTransitioning(true);
-      setCurrentIndex((prevIndex) => (prevIndex + 1) % works.length);
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % (works.length + 1));
     }
   };
 
@@ -49,8 +77,29 @@ const Slider = ({ title }: { title: string }) => {
     if (!isTransitioning) {
       setIsTransitioning(true);
       setCurrentIndex(
-        (prevIndex) => (prevIndex - 1 + works.length) % works.length,
+        (prevIndex) => (prevIndex - 1 + works.length + 1) % (works.length + 1),
       );
+    }
+  };
+
+  const removeSlide = (index: number) => {
+    setWorks((prevWorks) => prevWorks.filter((_, i) => i !== index));
+    if (currentIndex >= index && currentIndex > 0) {
+      setCurrentIndex((prevIndex) => prevIndex - 1);
+    }
+  };
+
+  const goToBeginning = () => {
+    if (!isTransitioning && currentIndex !== 0) {
+      setIsTransitioning(true);
+      setCurrentIndex(0);
+    }
+  };
+
+  const goToEnd = () => {
+    if (!isTransitioning && currentIndex !== works.length - 1) {
+      setIsTransitioning(true);
+      setCurrentIndex(works.length - 1);
     }
   };
 
@@ -62,58 +111,91 @@ const Slider = ({ title }: { title: string }) => {
   }, [isTransitioning]);
 
   return (
-    <div className="h-auto max-w-fit border-2 border-gray-300 p-14">
-      <h2 className="mb-16 w-fit border-b-2 border-borders-bottomBorder pb-2 text-2xl">
-        {title}
-      </h2>
-      <div className="relative">
-        <div className="overflow-hidden">
+    <div className={customStyles.container}>
+      <h2 className={customStyles.title}>{title}</h2>
+      <div className={customStyles.sliderContainer}>
+        <div className={customStyles.sliderContent}>
           <div
-            className="duration-600 flex transition-all ease-in-out"
+            className={customStyles.sliderWrapper}
             style={{
               transform: `translateX(-${currentIndex * 25}%)`,
               transition: "transform 0.6s cubic-bezier(0.25, 0.1, 0.25, 1)",
             }}
           >
-            {[...works, ...works, ...works].map((work, index) => (
+            {works.map((work, index) => (
               <div
                 key={index}
-                className="duration-600 w-[25%] flex-shrink-0 px-2 transition-all"
+                className={customStyles.slide}
                 style={{
-                  transform: `scale(${index % works.length === currentIndex % works.length ? 1 : 0.9})`,
-                  opacity:
-                    index % works.length === currentIndex % works.length
-                      ? 1
-                      : 0.7,
+                  transform: `scale(${index === currentIndex ? 1 : 0.9})`,
+                  opacity: index === currentIndex ? 1 : 0.7,
                 }}
               >
-                <div className="h-full w-80 overflow-auto border-2 border-gray-300 bg-white p-8">
-                  <h3 className="mb-2 font-bold">{work.title}</h3>
-                  <p className="text-sm">{work.content}</p>
+                <div className={customStyles.slideContent}>
+                  <button
+                    onClick={() => removeSlide(index)}
+                    className={customStyles.removeButton}
+                  >
+                    <Minus
+                      size={16}
+                      className="stroke-white duration-150 group-hover:stroke-black"
+                    />
+                  </button>
+                  <h3 className={customStyles.slideTitle}>{work.title}</h3>
+                  <p className={customStyles.slideText}>{work.content}</p>
                 </div>
               </div>
             ))}
+            <div
+              className={customStyles.slide}
+              style={{
+                transform: `scale(${currentIndex === works.length ? 1 : 0.9})`,
+                opacity: currentIndex === works.length ? 1 : 0.7,
+              }}
+            >
+              <div className={customStyles.addSlideDiv} onClick={onAddSlide}>
+                <Plus className={customStyles.addSlideIcon} />
+              </div>
+            </div>
           </div>
         </div>
         <button
           onClick={prevSlide}
-          className="absolute left-0 top-1/2 z-10 -translate-x-full -translate-y-1/2 transform rounded-full bg-white bg-opacity-50 p-2 transition-all hover:bg-opacity-75 focus:outline-none focus:ring-2 focus:ring-black"
+          className={`${customStyles.navButton} left-0 -translate-x-full`}
           disabled={isTransitioning}
         >
           <ChevronLeft size={24} />
         </button>
         <button
           onClick={nextSlide}
-          className="absolute right-0 top-1/2 z-10 -translate-y-1/2 translate-x-full transform rounded-full bg-white bg-opacity-50 p-2 transition-all hover:bg-opacity-75 focus:outline-none focus:ring-2 focus:ring-black"
+          className={`${customStyles.navButton} right-0 translate-x-full`}
           disabled={isTransitioning}
         >
           <ChevronRight size={24} />
         </button>
+      </div>
+      <div className={customStyles.navigationButtons}>
+        <Button
+          className={customStyles.endBeginButton}
+          onClick={goToBeginning}
+          type="button"
+          disabled={currentIndex === 0 || isTransitioning}
+        >
+          <ChevronsLeft className="mr-2" size={20} />
+          Go to Beginning
+        </Button>
+        <Button
+          className={customStyles.endBeginButton}
+          onClick={goToEnd}
+          type="button"
+          disabled={currentIndex === works.length - 1 || isTransitioning}
+        >
+          Go to End
+          <ChevronsRight className="ml-2" size={20} />
+        </Button>
       </div>
     </div>
   );
 };
 
 export default Slider;
-
-const customStyles = {};
