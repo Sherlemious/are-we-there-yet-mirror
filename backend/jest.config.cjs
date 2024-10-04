d706dee29c712f908fa50d4da4c7dec5a0c04b3f
Youssef Yasser <yousef.mostafa@student.guc.edu.eg>
2024-10-04 16:58:30 +0300

    fix jest in deployment

--- backend/jest.config.cjs
+++ backend/jest.config.cjs
@@ -4,7 +4,10 @@ module.exports = {
   roots: ['<rootDir>/tests'],
   testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
   transform: {
-    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.tsx?$': ['ts-jest',
+    {
+      tsconfig: 'tsconfig.test.json',
+    }]
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
