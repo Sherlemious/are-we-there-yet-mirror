52f26d65aa6b81e2cf2283a10caf2d4777521671
Mostafa Hisham <52044516+mostafahisham03@users.noreply.github.com>
2024-10-06 17:00:27 +0300

    style the form for editing and adding museums

--- frontend/src/modules/Museums/components/MuseumForm.tsx
+++ frontend/src/modules/Museums/components/MuseumForm.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useRef } from 'react';
 import { ModalRef } from './modal';
+import defaultPhoto from '../assets/defaultPhoto.png';
 
 interface MuseumFormProps {
   onSubmit?: (museumData: MuseumFormData) => void;
@@ -47,6 +48,9 @@ const MuseumForm: React.FC<MuseumFormProps> = ({ onSubmit, addModalRef, initialD
 
   const [pictures, setPictures] = useState<string[]>([]); // Separate state for file uploads
   const fileInputRef = useRef<HTMLInputElement | null>(null); // Create a ref for the file input
+  const [imagePreview, setImagePreview] = useState<string>(defaultPhoto); // State for image preview
+  const [imageIndex, setImageIndex] = useState(0); // State to keep track of the current image index
+
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     const { name, value } = e.target;
@@ -72,30 +76,36 @@ const MuseumForm: React.FC<MuseumFormProps> = ({ onSubmit, addModalRef, initialD
 
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const files = Array.from(e.target.files || []);
-    setPictures(files.map(file => URL.createObjectURL(file))); // Store the file URLs
+    if (files.length > 0) {
+      setPictures(files.map(file => URL.createObjectURL(file))); // Store the file URLs
+      setImageIndex(0); // Reset to the first image
+      setImagePreview(URL.createObjectURL(files[0])); // Display the first uploaded image
+    }
   };
+
 
   // Handling tags
   const handleTagChange = (index: number, e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
     const { name, value } = e.target;
     const updatedTags = [...formData.tags];
-    updatedTags[index] = value; 
-
+    updatedTags[index] = value;
+  
     setFormData((prevData) => ({
       ...prevData,
       tags: updatedTags,
     }));
   };
+  
   const addTag = () => {
     setFormData((prevData) => ({
       ...prevData,
       tags: [
         ...prevData.tags,
+        "", // Add an empty string as the new tag
       ],
     }));
   };
-
-
+  
   const removeTag = (index: number) => {
     setFormData((prevData) => ({
       ...prevData,
@@ -105,12 +115,23 @@ const MuseumForm: React.FC<MuseumFormProps> = ({ onSubmit, addModalRef, initialD
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    const formDataWithAttachments = { ...formData, pictures: [] };
+    const formDataWithAttachments = { ...formData, pictures: pictures, };
 
     if (onSubmit) {
       onSubmit(formDataWithAttachments);
     }
     addModalRef.current?.close();
+  };
+
+  const handleImageToggle = (direction: 'next' | 'prev') => {
+    setImageIndex((prevIndex) => {
+      if (direction === 'next') {
+        return (prevIndex + 1) % pictures.length; // Loop to the first image
+      } else {
+        return (prevIndex - 1 + pictures.length) % pictures.length; // Loop to the last image
+      }
+    });
+    setImagePreview(pictures[imageIndex]); // Update the preview with the selected image
   };
   // const resetForm = () => {
   //   setFormData({
@@ -142,127 +163,188 @@ const MuseumForm: React.FC<MuseumFormProps> = ({ onSubmit, addModalRef, initialD
   // };
 
   return (
-    <form onSubmit={handleSubmit} className="flex flex-col w-1/3 mx-auto">
-      <label htmlFor="name">Museum Name</label>
-      <input
-        type="text"
-        name="name"
-        value={formData.name}
-        onChange={handleInputChange}
-        placeholder="Museum Name"
-        className={styles.inputClass}
-      />
-      <label htmlFor="description">Description</label>
-      <textarea
-        name="description"
-        value={formData.description}
-        onChange={handleInputChange}
-        placeholder="Description"
-        className={styles.inputClass}
-      />
-      <label htmlFor="category">Category</label>
-      <input
-        type="text"
-        name="category"
-        value={formData.category}
-        onChange={handleInputChange}
-        placeholder="Category"
-        className={styles.inputClass}
-      />
-        {/* Tags Section */}
-        <h3 className="mt-4">Tags</h3>
-      {formData.tags.map((tag, index) => (
-        <div key={index} className="flex flex-col mb-2">
-          <label htmlFor={`tag-name-${index}`}>Tag</label>
+    <form onSubmit={handleSubmit} className="grid grid-cols-2 gap-4 w-7/8 mx-auto mt-4">
+      {/* Left Side */}
+      <div className="col-span-1 grid grid-cols-2 gap-4">
+        {/* Name (Full width) */}
+        <div className="col-span-2">
+          <label htmlFor="name" className="mb-2 block">Museum Name</label>
           <input
             type="text"
             name="name"
-            value={tag}
-            onChange={(e) => handleTagChange(index, e)}
-            placeholder="Tag Name"
+            value={formData.name}
+            onChange={handleInputChange}
+            placeholder="Museum Name"
             className={styles.inputClass}
           />
+        </div>
+  
+        {/* Description (Full width) */}
+        <div className="col-span-2">
+          <label htmlFor="description" className="mb-2 block">Description</label>
+          <textarea
+            name="description"
+            value={formData.description}
+            onChange={handleInputChange}
+            placeholder="Description"
+            className={`${styles.inputClass} h-24`} // Increase height for textarea
+          />
+        </div>
+  
+        {/* Category and Location (Side by side) */}
+        <div className="col-span-1">
+          <label htmlFor="category" className="mb-2 block">Category</label>
+          <input
+            type="text"
+            name="category"
+            value={formData.category}
+            onChange={handleInputChange}
+            placeholder="Category"
+            className={styles.inputClass}
+          />
+        </div>
+        <div className="col-span-1">
+          <label htmlFor="location" className="mb-2 block">Location</label>
+          <input
+            type="text"
+            name="location"
+            value={formData.location.name}
+            onChange={handleInputChange}
+            placeholder="Location"
+            className={styles.inputClass}
+          />
+        </div>
+  
+        {/* Opening Hours (Full width) */}
+        <div className="col-span-2">
+          <label htmlFor="opening_hours" className="mb-2 block">Opening Hours</label>
+          <input
+            type="text"
+            name="opening_hours"
+            value={formData.opening_hours}
+            onChange={handleInputChange}
+            placeholder="Opening Hours"
+            className={styles.inputClass}
+          />
+        </div>
+  
+        {/* Ticket Prices (Side by side) */}
+        <div className="col-span-1">
+          <label htmlFor="price-foreigner" className="mb-2 block">Ticket Price (Foreigner)</label>
+          <input
+            type="number"
+            name="foreigner"
+            value={formData.ticket_prices.foreigner}
+            onChange={handlePriceChange}
+            placeholder="Price for Foreigners"
+            className={styles.inputClass}
+          />
+        </div>
+        <div className="col-span-1">
+          <label htmlFor="price-native" className="mb-2 block">Ticket Price (Native)</label>
+          <input
+            type="number"
+            name="native"
+            value={formData.ticket_prices.native}
+            onChange={handlePriceChange}
+            placeholder="Price for Natives"
+            className={styles.inputClass}
+          />
+        </div>
+        <div className="col-span-1">
+          <label htmlFor="price-student" className="mb-2 block">Ticket Price (Student)</label>
+          <input
+            type="number"
+            name="student"
+            value={formData.ticket_prices.student}
+            onChange={handlePriceChange}
+            placeholder="Price for Students"
+            className={styles.inputClass}
+          />
+        </div>
+      </div>
+  
+      {/* Right Side */}
+      <div className="col-span-1 flex flex-col"> {/* Centering the column */}
+        {/* Picture Upload */}
+        <div>
+          <img 
+            src={imagePreview} 
+            alt="Preview" 
+            className="mt-4-gray-300 rounded-md w-full h-64 object-cover" 
+          />
+          {/* Conditionally render arrows if multiple pictures are uploaded */}
+          {pictures.length > 1 && (
+            <div className="flex justify-between mb-2">
+              <span 
+                onClick={() => handleImageToggle('prev')} 
+                className="cursor-pointer text-gray-500 hover:text-black"
+              >
+                &#9664; {/* Left arrow */}
+              </span>
+              <span 
+                onClick={() => handleImageToggle('next')} 
+                className="cursor-pointer text-gray-500 hover:text-black"
+              >
+                &#9654; {/* Right arrow */}
+              </span>
+            </div>
+          )}
+          <label htmlFor="pictures" className="mb-2 block">Upload Pictures</label>
+          <input
+            type="file"
+            multiple
+            onChange={handleFileChange}
+            className={styles.inputClass}
+            ref={fileInputRef}
+          />
+        </div>
+
+  
+        {/* Tags Section (Below Pictures) */}
+        <div className="mt-6">
+          <h3 className="mt-6 mb-4">Tags</h3>
+          {formData.tags.map((tag, index) => (
+            <div key={index} className="flex items-center mb-2">
+              <input
+                type="text"
+                name="tag"
+                value={tag}
+                onChange={(e) => handleTagChange(index, e)}
+                placeholder="Tag Name"
+                className={styles.inputClass}
+              />
+              <button
+                type="button"
+                onClick={() => removeTag(index)}
+                className="ml-2 bg-red-500 text-white rounded-md p-1"
+              >
+                Remove
+              </button>
+            </div>
+          ))}
           <button
             type="button"
-            onClick={() => removeTag(index)}
-            className="mt-2 bg-red-500 text-white rounded-md p-1"
+            onClick={addTag}
+            className="mt-4 bg-green-500 text-white rounded-md p-2"
           >
-            Remove Tag
+            Add Tag
           </button>
         </div>
-      ))}
-      <button
-        type="button"
-        onClick={addTag}
-        className="mt-2 bg-green-500 text-white rounded-md p-1"
-      >
-        Add Tag
-      </button>
-      <label htmlFor="opening_hours">Opening Hours</label>
-      <input
-        type="text"
-        name="opening_hours"
-        value={formData.opening_hours}
-        onChange={handleInputChange}
-        placeholder="Opening Hours"
-        className={styles.inputClass}
-      />
-      <label htmlFor="location">Location</label>
-      <input
-        type="text"
-        name="location"
-        value={formData.location.name}
-        onChange={handleInputChange}
-        placeholder="Location"
-        className={styles.inputClass}
-      />
-      <label htmlFor="price-foreigner">Ticket Price for a foreigner</label>
-      <input
-        type="number"
-        name="foreigner" // Unique name for the ticket price field
-        value={formData.ticket_prices.foreigner}
-        onChange={handlePriceChange}
-        placeholder="Price for Foreigners"
-        className={styles.inputClass}
-      />
-      <label htmlFor="price-native">Ticket Price for a native</label>
-      <input
-        type="number"
-        name="native" // Unique name for the ticket price field
-        value={formData.ticket_prices.native}
-        onChange={handlePriceChange}
-        placeholder="Price for Natives"
-        className={styles.inputClass}
-      />
-      <label htmlFor="price-student">Ticket Price for a student</label>
-      <input
-        type="number"
-        name="student" // Unique name for the ticket price field
-        value={formData.ticket_prices.student}
-        onChange={handlePriceChange}
-        placeholder="Price for Students"
-        className={styles.inputClass}
-      />
-      
-    
-      {/* File Upload */}
-      <label htmlFor="pictures">Upload Pictures</label>
-      <input
-        type="file"
-        multiple // Allow multiple file uploads
-        onChange={handleFileChange}
-        className={styles.inputClass}
-        ref={fileInputRef} // Attach the ref here
-      />
-      <button type="submit" className={styles.button}>
-        Submit
-      </button>
+      </div>
+  
+      {/* Submit Button (Full width) */}
+      <div className="col-span-2 flex justify-center">
+        <button type="submit" className={`${styles.button} w-1/2 p-4`}>
+          Submit
+        </button>
+      </div>
     </form>
-  );
+  );  
 };
 
 const styles = {
-  inputClass: 'border border-gray-300 rounded-md p-2 mb-2',
+  inputClass: 'border border-gray-300 rounded-md p-2 mb-4 w-full',
   button: 'bg-gray-500 text-white rounded-md p-2',
 };
 
