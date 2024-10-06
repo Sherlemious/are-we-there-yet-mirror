bfe5e4696bbadbd18ed53f9ad590dce568ac1649
Ahmed Gado <ahmedgado0100@gmail.com>
2024-10-06 19:16:33 +0300

    feat: try an api that worked

--- frontend/src/modules/shared/utils/map.tsx
+++ frontend/src/modules/shared/utils/map.tsx
@@ -4,13 +4,13 @@ import { useMemo } from 'react';
 export default function Map() {
   const center = useMemo(() => ({ lat: 50, lng: 50 }), []);
 
-  console.log(import.meta.env.VITE_GOOGLE_MAPS_API_KEY);
-
   return (
     <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY!}>
-      <VisMap center={center} zoom={10}>
-        <Marker position={center} />
-      </VisMap>
+      <div className='w-full h-full'>
+        <VisMap center={center} zoom={10}>
+          <Marker position={center} />
+        </VisMap>
+      </div>
     </APIProvider>
   );
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   content: ['./index.html', './src/**/*.{html,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        text: {
+          primary: '#111827',
+          white: '#ffffff',
+        },
+        borders: {
+          primary: '#d1d5db',
+          bottom: '#71717a',
+        },
+        background: {
+          button: '#6b7280',
+        },
+      },
+    },
   },
   plugins: [],
 };
