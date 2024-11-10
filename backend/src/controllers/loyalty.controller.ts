c4d22f48dbd635f90a146d13a5053898f4c64f5d
Yousefyasser <yousef.mostafa@student.guc.edu.eg>
2024-11-10 15:03:48 +0200

    feat: remove getLoyaltyLevel endpoint and add it to user model (#64)

--- backend/src/controllers/loyalty.controller.ts
+++ backend/src/controllers/loyalty.controller.ts
@@ -8,20 +8,23 @@ import { UserType } from '../types/User.types';
 async function redeemPoints(req: Request, res: Response) {
   try {
     const userId: string = req.user.userId;
-    const user = await userRepo.findUserById(userId);
-    if (!user) throw new Error('User not found');
     const { points } = req.body;
+
+    const user = await userRepo.findUserById(userId);
+
+    if (!user) throw new Error('User not found');
+
     if (user.loyalty_points < points) {
       throw new Error('Insufficient points');
     }
+
     user.loyalty_points -= points;
-    if (!user.wallet) {
-      user.wallet = 0;
-    }
+    user.loyalty_level = getLoyaltyLevel(user.loyalty_points);
     user.wallet += points * 0.01;
 
     // Update user
     await userRepo.updateUser(userId, user as UserType);
+
     res.status(ResponseStatusCodes.OK).json({ message: 'Points redeemed successfully' });
   } catch (error: any) {
     logger.error(`Error redeeming points: ${error.message}`);
@@ -29,26 +32,18 @@ async function redeemPoints(req: Request, res: Response) {
   }
 }
 
-async function getLoyaltyLevel(req: Request, res: Response) {
-  try {
-    const userId: string = req.user.userId;
-    const user = await userRepo.findUserById(userId);
-    if (!user) throw new Error('User not found');
-    const points = user.loyalty_points;
-    let level = 0;
-    // 100k points
-    if (points >= 1000000) {
-      level = 3;
-    } else if (points >= 500000) {
-      level = 2;
-    } else if (points >= 100000) {
-      level = 1;
-    }
-    res.status(ResponseStatusCodes.OK).json({ message: 'Loyalty level fetched successfully', data: { level } });
-  } catch (error: any) {
-    logger.error(`Error fetching loyalty level: ${error.message}`);
-    res.status(ResponseStatusCodes.BAD_REQUEST).json({ message: error.message });
+function getLoyaltyLevel(points: number): number {
+  let level: number = 0;
+
+  if (points >= 1000000) {
+    level = 3;
+  } else if (points >= 500000) {
+    level = 2;
+  } else if (points >= 100000) {
+    level = 1;
   }
+
+  return level;
 }
 
-export { redeemPoints, getLoyaltyLevel };
+export { redeemPoints };
--- backend/src/routes/tourist.route.ts
+++ backend/src/routes/tourist.route.ts
@@ -1,9 +1,8 @@
 import { Router } from 'express';
 const router = Router();
 
-import { getLoyaltyLevel, redeemPoints } from '../controllers/loyalty.controller';
+import { redeemPoints } from '../controllers/loyalty.controller';
 
 router.post('/redeem', redeemPoints);
-router.get('/loyalty', getLoyaltyLevel);
 
 export default router;
