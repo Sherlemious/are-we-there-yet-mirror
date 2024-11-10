45c85f0c6f6c6e1062328af37924e3dbfc39ad51
Youssef Yasser <yousef.mostafa@student.guc.edu.eg>
2024-11-10 23:19:36 +0200

    hotfix: category in itinerary is not an array

--- backend/src/database/models/itinerary.model.ts
+++ backend/src/database/models/itinerary.model.ts
@@ -10,7 +10,7 @@ const itinerarySchema = new Schema(
       required: true,
     },
     category: {
-      type: [{ type: Schema.Types.ObjectId, ref: 'category' }],
+      type: { type: Schema.Types.ObjectId, ref: 'category' },
       required: true,
     },
     active: {
