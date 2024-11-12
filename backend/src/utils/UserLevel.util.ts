6fb5c8c7b11e801aa642cddc7862b6ee02388714
Yousefyasser <yousef.mostafa@student.guc.edu.eg>
2024-11-12 11:44:44 +0200

    hotfix: modify loyalty level based on points

--- backend/src/utils/UserLevel.util.ts
+++ backend/src/utils/UserLevel.util.ts
@@ -2,12 +2,10 @@ export default function getUserLevel(points: number): number {
   try {
     let level = 1;
     // 100k points
-    if (points >= 1000000) {
+    if (points >= 500000) {
       level = 3;
-    } else if (points >= 500000) {
-      level = 2;
     } else if (points >= 100000) {
-      level = 1;
+      level = 2;
     }
 
     return level;
