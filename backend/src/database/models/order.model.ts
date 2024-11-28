4b7628ff1fb7f71a6a56fd89b6f6c6bc889a17b6
Youssef Yasser <youssef.yasser@robustastudio.com>
2024-11-28 20:07:07 +0200

    feat: implement order schema and get orders endpoint

---
+++ backend/src/database/models/order.model.ts
@@ -0,0 +1,40 @@
+import { model, Schema } from 'mongoose';
+
+const orderSchema = new Schema(
+  {
+    products: [
+      {
+        product: {
+          type: Schema.Types.ObjectId,
+          ref: 'Product',
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+        },
+      },
+    ],
+    totalPrice: {
+      type: Number,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ['pending', 'completed', 'cancelled'],
+      default: 'pending',
+    },
+    created_by: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Order = model('Order', orderSchema);
+
+export { Order };
